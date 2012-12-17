518e4ef296ec1d0394a60b9211a4b2d4df16f0e5
christophstrobl <j73x73r@gmail.com>
2012-12-17 06:40:29 +0100

    DATASOLR-29 - added convenience methods for 'set', 'add', 'inc' updates

--- src/main/java/org/springframework/data/solr/core/query/PartialUpdate.java
+++ src/main/java/org/springframework/data/solr/core/query/PartialUpdate.java
@@ -44,12 +44,54 @@ public class PartialUpdate implements Update {
 		return this.idField;
 	}
 
+	/**
+	 * Add field with given name and value to the fields to be updated. Default {@link UpateAction} will be
+	 * {@link UpateAction.SET}.
+	 * 
+	 * @param fieldName
+	 * @param value
+	 */
 	public void add(String fieldName, Object value) {
 		add(new SimpleUpdateField(fieldName, value));
 	}
 
+	/**
+	 * Add {@link UpdateField} to the list of fields to be updated
+	 * 
+	 * @param field
+	 */
 	public void add(UpdateField field) {
 		this.updates.add(field);
+	}
+
+	/**
+	 * Add field with given name and value using {@link UpateAction.ADD} to the fields to be updated.
+	 * 
+	 * @param fieldName
+	 * @param value
+	 */
+	public void addValueToField(String fieldName, Object value) {
+		add(new SimpleUpdateField(fieldName, value, UpdateAction.ADD));
+	}
+
+	/**
+	 * Add field with given name and value using {@link UpateAction.SET} to the fields to be updated.
+	 * 
+	 * @param fieldName
+	 * @param value
+	 */
+	public void setValueOfField(String fieldName, Object value) {
+		add(new SimpleUpdateField(fieldName, value, UpdateAction.SET));
+	}
+
+	/**
+	 * Add field with given name and value using {@link UpateAction.INC} to the fields to be updated.
+	 * 
+	 * @param fieldName
+	 * @param value
+	 */
+	public void increaseValueOfField(String fieldName, Object value) {
+		add(new SimpleUpdateField(fieldName, value, UpdateAction.INC));
 	}
 
 	@Override
