dc24dc35dcf11a5606592543bb7fc6dfc79e7102
christophstrobl <j73x73r@gmail.com>
2013-05-13 05:40:53 +0200

    DATASOLR-73 - Upgrade to Solr 4.3.0

--- src/test/java/org/springframework/data/solr/AbstractITestWithEmbeddedSolrServer.java
+++ src/test/java/org/springframework/data/solr/AbstractITestWithEmbeddedSolrServer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012 the original author or authors.
+ * Copyright 2012 - 2013 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -50,6 +50,11 @@ public abstract class AbstractITestWithEmbeddedSolrServer {
 	public static void cleanDataInSolr() throws SolrServerException, IOException {
 		solrServer.deleteByQuery("*:*");
 		solrServer.commit();
+	}
+
+	@AfterClass
+	public static void shutdown() {
+		solrServer.shutdown();
 	}
 
 	public ExampleSolrBean createDefaultExampleBean() {
