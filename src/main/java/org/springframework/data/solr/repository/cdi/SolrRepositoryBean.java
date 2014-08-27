0b898c6c7278919aadedb79fe6dc905d9cc4e6a1
Mark Paluch <mpaluch@paluch.biz>
2014-08-27 10:22:41 +0200

    DATASOLR-195 Custom repository implementations are not picked up when using CDI

--- src/main/java/org/springframework/data/solr/repository/cdi/SolrRepositoryBean.java
+++ src/main/java/org/springframework/data/solr/repository/cdi/SolrRepositoryBean.java
@@ -23,6 +23,7 @@ import javax.enterprise.inject.spi.Bean;
 import javax.enterprise.inject.spi.BeanManager;
 
 import org.springframework.data.repository.cdi.CdiRepositoryBean;
+import org.springframework.data.repository.config.CustomRepositoryImplementationDetector;
 import org.springframework.data.solr.core.SolrOperations;
 import org.springframework.data.solr.repository.support.SolrRepositoryFactory;
 import org.springframework.util.Assert;
@@ -44,12 +45,12 @@ public class SolrRepositoryBean<T> extends CdiRepositoryBean<T> {
 	 * @param qualifiers must not be {@literal null}.
 	 * @param repositoryType must not be {@literal null}.
 	 * @param beanManager must not be {@literal null}.
-	 * @param customImplementationBean the bean for the custom implementation of the
-	 *          {@link org.springframework.data.repository.Repository}, can be {@literal null}.
+	 * @param detector detector for the custom {@link org.springframework.data.repository.Repository} implementations
+   	 *          {@link CustomRepositoryImplementationDetector}, can be {@literal null}.
 	 */
 	public SolrRepositoryBean(Bean<SolrOperations> operations, Set<Annotation> qualifiers, Class<T> repositoryType,
-			BeanManager beanManager, Bean<?> customImplementationBean) {
-		super(qualifiers, repositoryType, beanManager, customImplementationBean);
+			BeanManager beanManager, CustomRepositoryImplementationDetector detector) {
+		super(qualifiers, repositoryType, beanManager, detector);
 
 		Assert.notNull(operations, "Cannot create repository with 'null' for SolrOperations.");
 		this.solrOperationsBean = operations;
