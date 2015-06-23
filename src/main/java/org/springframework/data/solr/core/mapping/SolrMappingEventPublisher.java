1590dcd656b7f6542553d1c626cdb2bbdcc06fbe
Christoph Strobl <cstrobl@pivotal.io>
2015-06-23 09:20:05 +0200

    DATASOLR-243 - Ensure Spring Framework 4.2 compatibility.
    Removed obsolete generics in SolrPersistentEntitySchemaCreator to make sure MappingContextEvents are delivered to the listener on Spring 4.2.

--- src/main/java/org/springframework/data/solr/core/mapping/SolrMappingEventPublisher.java
+++ src/main/java/org/springframework/data/solr/core/mapping/SolrMappingEventPublisher.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 the original author or authors.
+ * Copyright 2014-2015 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,16 +15,25 @@
  */
 package org.springframework.data.solr.core.mapping;
 
+import java.lang.reflect.Constructor;
+
+import org.springframework.beans.BeanUtils;
 import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.data.mapping.context.MappingContextEvent;
 import org.springframework.data.solr.core.schema.SolrPersistentEntitySchemaCreator;
+import org.springframework.util.Assert;
+import org.springframework.util.ClassUtils;
 
 /**
  * @author Christoph Strobl
  * @since 1.3
  */
 public class SolrMappingEventPublisher implements ApplicationEventPublisher {
+
+	static final String SPRING_42_PAYLOAD_APPLICATION_EVENT_CLASS_NAME = "org.springframework.context.PayloadApplicationEvent";
+	static final boolean SPRING_42_PAYLOAD_APPLICATION_EVENT_PRESENT = ClassUtils.isPresent(
+			SPRING_42_PAYLOAD_APPLICATION_EVENT_CLASS_NAME, null);
 
 	SolrPersistentEntitySchemaCreator schmeaCreator;
 
@@ -44,5 +53,44 @@ public class SolrMappingEventPublisher implements ApplicationEventPublisher {
 			this.schmeaCreator
 					.onApplicationEvent((MappingContextEvent<SolrPersistentEntity<?>, SolrPersistentProperty>) event);
 		}
+	}
+
+	/* 
+	 * (non-Javadoc)
+	 * @see org.springframework.context.ApplicationEventPublisher#publishEvent(java.lang.Object)
+	 */
+	public void publishEvent(Object event) {
+
+		Assert.notNull(event, "Event to publish must not be null!");
+
+		publishEvent(initGenericApplicationEvent(event));
+	}
+
+	@SuppressWarnings("serial")
+	private ApplicationEvent initGenericApplicationEvent(Object event) {
+
+		ApplicationEvent applicationEvent = null;
+
+		if (event instanceof ApplicationEvent) {
+			applicationEvent = ((ApplicationEvent) event);
+		}
+
+		if (SPRING_42_PAYLOAD_APPLICATION_EVENT_PRESENT) {
+
+			try {
+
+				Constructor<?> ctor = ClassUtils.getConstructorIfAvailable(
+						ClassUtils.forName(SPRING_42_PAYLOAD_APPLICATION_EVENT_CLASS_NAME, this.getClass().getClassLoader()),
+						Object.class, Object.class);
+				applicationEvent = (ApplicationEvent) BeanUtils.instantiateClass(ctor, (Object) this, event);
+			} catch (Exception e) {
+				// ignore and fall back to ApplicationEvent
+			}
+		}
+
+		if (applicationEvent == null) {
+			applicationEvent = new ApplicationEvent(event) {};
+		}
+		return applicationEvent;
 	}
 }
