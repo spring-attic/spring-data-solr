04d2f8b4b40b2d84b9a382bd5277f8bc66c56c15
Francisco Spaeth <francisco.spaeth@gmail.com>
2016-05-28 16:20:41 -0300

    DATASOLR-310 - Limit Parameter on GroupOptions is not included in Query when -1
    GroupOptions limit can be set to a negative value indicating unlimited results for each group.
    
    Original Pull Request: #66

--- src/main/java/org/springframework/data/solr/core/DefaultQueryParser.java
+++ src/main/java/org/springframework/data/solr/core/DefaultQueryParser.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012 - 2015 the original author or authors.
+ * Copyright 2012 - 2016 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -216,7 +216,7 @@ public class DefaultQueryParser extends QueryParserBase<SolrDataQuery> {
 			solrQuery.add(GroupParams.GROUP_CACHE_PERCENTAGE, String.valueOf(groupOptions.getCachePercent()));
 		}
 
-		if (groupOptions.getLimit() != null && groupOptions.getLimit() >= 0) {
+		if (groupOptions.getLimit() != null) {
 			solrQuery.set(GroupParams.GROUP_LIMIT, groupOptions.getLimit());
 		}
 
